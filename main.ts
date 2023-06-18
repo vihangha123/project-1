9895a70c091668f4c17c3b40e7f670317bb9a9a2
vihangha123 <136943486+vihangha123@users.noreply.github.com>
2023-06-18 07:58:55 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,4 +7,20 @@ basic.forever(function () {
         . . . . .
         `)
     basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
 })
